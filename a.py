1f141a96bdb7fb113be280099b13b7f7a8be4e4b
TrungPhan <finaloppo@gmail.com>
2021-07-10 23:12:50 +0700

    Update a.py

--- a.py
+++ a.py
@@ -11,12 +11,13 @@ class TextureManager:
         img = Image.open(file_path)
         img = ImageTk.PhotoImage(img)
         _canvas.create_image(0, 0, anchor=NW, image=img) 
-        root.mainloop()
+        
+        
 
 if __name__=='__main__':
     root = Tk()
     canvas = Canvas(root, width=400,height=800)
-    
+    root.mainloop()
     canvas.pack()
     TextureManager.load_image('/home/tucuman/FlappyBird-JavaScript/images/new_bg.png',canvas)
     TextureManager.load_image('/home/tucuman/FlappyBird-JavaScript/images/anh/bird.png',canvas)
